0a3cc0e2e8b12b50f6f5a82df2f311a167381323
Nikita <nikita.shaban7@gmail.com>
2020-01-17 09:59:39 +0300

    Start

---
+++ src/ducks/auth.js
@@ -0,0 +1,20 @@
+// const
+
+//initial state
+
+const initialState = {
+  a: 100
+};
+
+//reducer
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+    default: {
+      return state;
+    }
+  }
+};
+
+//actions
+
+//action creators
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+import auth from "../ducks/auth";
+
+export default combineReducers({ auth });
