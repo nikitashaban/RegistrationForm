bc6646a6b384a217fa667e3c5cfd68fe16ba16be
Nikita <nikita.shaban7@gmail.com>
2020-01-18 11:34:46 +0300

    paginationAdded

--- src/components/Articles/Article/Article.js
+++ src/components/Articles/Article/Article.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 
-const Article = ({ text, title, id }) => {
+const Article = ({ posts }) => {
   return (
-    <Card>
-      <h3>{title}</h3>
-      <p>Id: {id}</p>
-      <Card.Body>{text}</Card.Body>
-    </Card>
+    <div>
+      {posts.map(info => (
+        <Card className="container">
+          <h3 className="text-primary">{info.title}</h3>
+          <p>Id: {info.id}</p>
+          <Card.Body>{info.text}</Card.Body>
+        </Card>
+      ))}
+    </div>
   );
 };
 
--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -1,21 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Search from "../Search/Search";
+import Pagination from "../../helpers/Pagination";
 import { articleFetchData } from "../../ducks/auth";
 import { useDispatch, useSelector } from "react-redux";
 import Article from "../Articles/Article/Article";
 
 const Articles = props => {
   const data = useSelector(state => state.auth.results);
-  const text = data.map(info => <Article title={info.title} text={info.text} id={info.id} />);
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(articleFetchData());
   }, [dispatch]);
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage] = useState(5);
+  //Get current posts
+  const indexOfLastPost = currentPage * postPerPage;
+  const indeOfFirstPost = indexOfLastPost - postPerPage;
+  const currentPosts = data.slice(indeOfFirstPost, indexOfLastPost);
+
+  //Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
   return (
     <React.Fragment>
       <Search />
-      {text}
+      <Article posts={currentPosts} />
+      <Pagination postsPerPage={postPerPage} totalPosts={data.length} paginate={paginate} />
     </React.Fragment>
   );
 };
